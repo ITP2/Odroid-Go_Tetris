d48d8a6f2138d7ca5010d26231918a62912a00f5
OlliVino <63098101+OlliVino@users.noreply.github.com>
2020-04-28 10:13:20 +0200

    Add files via upload
    Tetris_ASCII Versuch

---
+++ Game.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Game.h"
+
+/*
+Game::Game(size_t height, size_t width):
+    isGameOver(false), actualBrick(NULL), field(height, width){
+
+}
+
+void Game::print() const{
+    this->field.print();
+}
+*/
---
+++ Game.h
@@ -0,0 +1,50 @@
+#ifndef GAME_H_INCLUDED
+#define GAME_H_INCLUDED
+#include <iostream>
+#include <cstddef>
+#include <vector>
+#include "Field.h"
+#include "Tetromino.h"
+
+/*
+class Game{
+    public:
+
+        struct Cursor{
+            Point()
+            : x(0), y(0){
+            }
+
+            void setY(unsigned short y){
+                this->y = y;
+            }
+            void setX(unsigned short x){
+                this->x = x;
+            }
+            void getY() const{
+                return this->y;
+            }
+            void getX() const{
+                return this->x;
+            }
+
+            unsigned short x;
+            unsigned short y;
+        };
+
+        Game(size_t height, size_t width);
+
+        void print() const;
+
+    private:
+        Field field;
+
+        Tetromino *actualBrick;
+        Point Cursor; in Arbeit
+
+        bool isGameOver;
+
+};
+*/
+
+#endif // GAME_H_INCLUDED
